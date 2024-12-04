852d3ea30e5d28cf87c13d1ac3f3cd84e23375fe
xswitch <bivarmong@gmail.com>
2024-12-04 11:17:16 +0100

    feat: removed boilerplate feat: set up react router feat: set up folder structure

---
+++ src/components/shop/shop.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is the shop</h1>
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}
+
+export default Shop
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from './components/home/home'
+import Shop from './components/shop/shop'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/shop',
+    element: <Shop />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
