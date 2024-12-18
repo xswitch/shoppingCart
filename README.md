bda32460af0437fff127a31dfbf033650a0389c4
xswitch <bivarmong@gmail.com>
2024-12-18 15:42:06 +0100

    feat: clicking outside of cart closes it

--- README.md
+++ README.md
@@ -20,7 +20,12 @@ TODO
  > Show sum of items if more than one of an item
 
 
-Created the cart modal
-showing empty if empty
-todo
-show items
+Show total price & amount up top and a checkout button on bottom
+fix: new items bubble pops up if item are added while cart is open
+-> close cart if user clicks anywhere but inside the cart
+ > Use the same handleClick as clicking the cart does when clicking outside
+
+
+Bugs
+cart sometimes doesn't open on click
+Attach event listener on open and remove on close
--- src/components/app/cart/cart.jsx
+++ src/components/app/cart/cart.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import style from "./cart.module.css";
-import { Car, ShoppingCart } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import CartItem from "../cartItem/cartItem";
 
 const Cart = ({ cart, products }) => {
   const [active, setActive] = useState(false);
   const [previousItemAmount, setPreviousItemAmount] = useState(0);
+  const cartRef = useRef(null);
+
+  useEffect(() => {
+    const outsideClick = (event) => {
+      if (
+        active &&
+        !cartRef.current.contains(event.target) &&
+        cartRef.current
+      ) {
+        setActive(false);
+      }
+    };
+    document.addEventListener("mousedown", outsideClick);
+    return () => {
+      document.removeEventListener("mousedown", outsideClick);
+    };
+  }, [active]);
 
   const productsInCart = Object.keys(cart).map((key) => {
     let product = products.filter((entry) => `${entry.id}` === key);
@@ -21,32 +38,29 @@ const Cart = ({ cart, products }) => {
   const itemAmount = productsInCart.reduce((prev, cur) => cur.amount + prev, 0);
 
   const handleClick = () => {
-    setActive((active) => {
-      if (active) {
-        return false;
-      } else {
-        setPreviousItemAmount(itemAmount);
-        return true;
-      }
-    });
+    if (!active) {
+      setActive(true);
+      setPreviousItemAmount(itemAmount);
+    }
   };
+
   return (
     <div className={style.container}>
       <ShoppingCart
         color={active ? "orange" : "white"}
         size={24}
         className={style.cart}
-        onClick={handleClick}
+        onClick={() => handleClick()}
       />
       {itemAmount > previousItemAmount && (
         <div className={style.newItems}>{itemAmount - previousItemAmount}</div>
       )}
       {active && (
-        <div className={style.cartContainer}>
+        <div className={style.cartContainer} ref={cartRef}>
           {productsInCart.length > 0 ? (
-              productsInCart.map((product) => (
-                <CartItem key={crypto.randomUUID()} product={product} />
-              ))
+            productsInCart.map((product) => (
+              <CartItem key={crypto.randomUUID()} product={product} />
+            ))
           ) : (
             <div className={style.empty}>
               <h4>Empty</h4>
