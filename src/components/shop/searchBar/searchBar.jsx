ddb648cd4e22db7e810d5e6cb97112c8470ceb4c
xswitch <bivarmong@gmail.com>
2024-12-21 11:47:56 +0100

    feat: searching

--- src/components/shop/searchBar/searchBar.jsx
+++ src/components/shop/searchBar/searchBar.jsx
@@ -1,10 +1,10 @@
 import { Search } from 'lucide-react'
 import style from './searchBar.module.css'
 
-export default function SearchBar() {
+export default function SearchBar({changeSearch}) {
   return (
-    <div>
-      <input type="search" name="searchBar" id="searchBar" />
+    <div className={style.container}>
+      <input type="search" name="searchBar" id="searchBar" placeholder='Search...' onChange={(e) => changeSearch(e)} />
       <Search />
     </div>
   )
--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Categories from "./categories/categories";
 import ProductCard from "./productCard/productCard";
 import SearchBar from "./searchBar/searchBar";
@@ -6,18 +7,42 @@ import { useOutletContext } from "react-router-dom";
 
 const Shop = () => {
   const [data, loading, error, addToCart] = useOutletContext();
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(null);
+
+  useEffect(() => {
+    setFilteredProducts(
+      data.filter((product) => String(product.title).includes(search))
+    );
+  }, [search, data]);
+
+  const changeSearch = (input) => {
+    console.log(input.target.value);
+    if (input.target.value === "") {
+      setSearch("");
+    } else {
+      setSearch(input.target.value);
+    }
+  };
+
   if (error) return <div>{error}</div>;
   return (
     <div className={style.container}>
       <div className={style.products}>
         {loading && <h1>Loading...</h1>}
-        {data &&
-          data.map((product) => {
-            return <ProductCard key={crypto.randomUUID()} product={product} addToCart={addToCart} />;
+        {filteredProducts &&
+          filteredProducts.map((product) => {
+            return (
+              <ProductCard
+                key={crypto.randomUUID()}
+                product={product}
+                addToCart={addToCart}
+              />
+            );
           })}
       </div>
       <div className={style.sidebar}>
-        <SearchBar />
+        <SearchBar changeSearch={changeSearch} />
         {loading ? <h4>Loading...</h4> : <Categories products={data} />}
       </div>
     </div>
